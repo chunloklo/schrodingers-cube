71ecb36223b4628e9dfc1403c8a6d06f38315ea1
chunloklo <clo42@gatech.edu>
2019-12-31 17:58:22 -0500

    change favicon

--- src/components/bio.js
+++ src/components/bio.js
@@ -14,7 +14,7 @@ import { rhythm } from "../utils/typography"
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      avatar: file(absolutePath: { regex: "/profile-pic-circle.png/" }) {
         childImageSharp {
           fixed(width: 60, height: 60) {
             ...GatsbyImageSharpFixed
