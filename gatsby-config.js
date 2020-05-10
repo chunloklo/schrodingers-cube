a849c763835e437e94a58a2814b00a45a5d05969
chunloklo <clo42@gatech.edu>
2020-05-10 19:03:16 -0400

    Riot blog update!

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,13 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "nofollow"
+            }
+          },
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
